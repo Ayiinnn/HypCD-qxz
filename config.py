fb3480f9ac9ffbd5056c54e0fff30e771bb1ada8
Ayiinnn <qiuxz1026@gmail.com>
2026-05-19 10:52:52 +0800

    Set up cloud training config

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # -----------------
 # DATASET ROOTS
 # -----------------
-cub_root = '{DATA_PATH}/cub'
-car_root = '{DATA_PATH}/cars'
-aircraft_root = '{DATA_PATH}/fgvc-aircraft-2013b'
-cifar_10_root = '{DATA_PATH}/cifar10'
-cifar_100_root = '{DATA_PATH}/cifar100'
-imagenet_root = '{DATA_PATH}/imagenet'
-herbarium_dataroot = '{DATA_PATH}/herbarium_19'
-pets_root = '{DATA_PATH}/oxford_pets'
+cub_root = '/data/datasets/cub'
+car_root = '/data/datasets/cars'
+aircraft_root = '/data/datasets/fgvc-aircraft-2013b'
+cifar_10_root = '/data/datasets/cifar10'
+cifar_100_root = '/data/datasets/cifar100'
+imagenet_root = '/data/datasets/imagenet'
+herbarium_dataroot = '/data/datasets/herbarium_19'
+pets_root = '/data/datasets/oxford_pets'
 
 # OSR Split dir
 osr_split_dir = 'data/ssb_splits'
@@ -16,11 +16,11 @@ osr_split_dir = 'data/ssb_splits'
 # -----------------
 # OTHER PATHS
 # -----------------
-dino_pretrain_path = '{PRETRAINED_PATH}/dino_vitb16_pretrain.pth'
-dinov2_pretrain_path = '{PRETRAINED_PATH}/dinov2_vitb14_reg4_pretrain.pth'
+dino_pretrain_path = '/data/pretrained/dino_vitb16_pretrain.pth'
+dinov2_pretrain_path = '/data/pretrained/dinov2_vitb14_reg4_pretrain.pth'
 feature_extract_dir = 'osr_novel_categories/extracted_features_public_impl'
 
 # -----------------
 # OTHER PATHS
 # -----------------
-exp_root = '{PROJECT_PATH}/HypCD/dev_outputs'  # All logs and checkpoints will be saved here
+exp_root = '/data/projects/HypCD/dev_outputs'  # All logs and checkpoints will be saved here
